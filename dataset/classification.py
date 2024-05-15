bfc8c7134cfea96c7d9de20feec620b5679ad5ae
Sisha <spalanevich@gmail.com>
2024-05-15 15:23:57 +0300

    add experiments for jannis dataset

--- dataset/classification.py
+++ dataset/classification.py
@@ -2,7 +2,7 @@ import numpy as np
 import numpy.typing as npt
 import typing as tp
 from sklearn.preprocessing import QuantileTransformer
-from sklearn.datasets import fetch_covtype
+from sklearn.datasets import fetch_covtype, fetch_openml
 from torch.utils.data import DataLoader
 from .common import TaskType, TaskConfig, train_val_test_split, get_data_loaders
 
@@ -24,17 +24,23 @@ def preprocess_features(data: npt.NDArray[np.float32], split: tp.Dict[str, npt.N
 
 
 def preprocess_target(target: npt.NDArray[np.float32], split: tp.Dict[str, npt.NDArray[np.int32]]):
-    return {split_name: target[split_idx] - 1 for split_name, split_idx in split.items()} # subtracting 1 to make classes be in [0, num_classes) range
+    return {split_name: target[split_idx] for split_name, split_idx in split.items()} # subtracting 1 to make classes be in [0, num_classes) range
 
 
-def fetch_data(batch_size: int = 1024, num_workers: int = 4) -> tp.Tuple[TaskConfig, tp.Dict[str, DataLoader]]:
-    dataset = fetch_covtype()
-    n_samples, n_features = dataset['data'].shape
-    n_classes = np.unique(dataset['target']).shape[0]
+def fetch_data(name: tp.Literal['co', 'ja'], batch_size: int = 256, num_workers: int = 4) -> tp.Tuple[TaskConfig, tp.Dict[str, DataLoader]]:
+    if name == 'co':
+        dataset = fetch_covtype()
+        data, target = dataset['data'], dataset['target']
+    else:
+        dataset = fetch_openml(name='jannis', version=1)
+        data, target = dataset['data'].to_numpy(), dataset['target'].to_numpy()
+
+    n_samples, n_features = data.shape
+    n_classes = np.unique(target).shape[0]
 
     split = train_val_test_split(n_samples)
-    preprocessed_features = preprocess_features(dataset['data'].astype(np.float32), split)
-    preprocessed_target = preprocess_target(dataset['target'].astype(np.int64), split)
+    preprocessed_features = preprocess_features(data.astype(np.float32), split)
+    preprocessed_target = preprocess_target(target.astype(np.int64), split)
 
     loaders = get_data_loaders(preprocessed_features, preprocessed_target, batch_size=batch_size, num_workers=num_workers)
     return TaskConfig(TaskType.CLASSIFICATION, n_features, n_classes), loaders
